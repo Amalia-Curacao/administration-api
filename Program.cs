4bb4e0a5c25fc8c93a184b8e47845755ca2b3254
Emile <emilemetry@outlook.com>
2023-12-29 18:05:58 -0400

    test

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (true | app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
