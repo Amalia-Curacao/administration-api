65f3694bdafd5e53381bdb0931809c4acbd151bd
Emile <emilemetry@outlook.com>
2023-12-29 14:46:16 -0400

    - Closed endpoint currently not in use for safety. - Added guest/set endpoint to set the guests.

--- Controllers/GuestController.cs
+++ Controllers/GuestController.cs
@@ -22,11 +22,18 @@ public class GuestController : Controller
 
 	/// <summary> Api endpoint for getting all guests from a reservation in the database.</summary>
 	/// <returns> 
-	/// Status 200 (OK) with all guests  from a reservation in the database.
+	/// Status 200 (OK) with all guests from a reservation in the database.
 	/// </returns>
 	[HttpGet($"[controller]/[action]/{{{nameof(Guest.ReservationId)}}}")]
 	public async Task<ObjectResult> GetReservationGuests([FromRoute] int ReservationId)
-		=> Ok((await _crud.GetAll()).Where(g => g.ReservationId == ReservationId));
+		=> Ok(await GetGuestsWithReservationId(ReservationId));
+
+	/// <summary> Api endpoint for getting all guests from a reservation in the database.</summary>
+	/// <returns>
+	/// Status 200 (OK) with all guests from a reservation in the database.
+	/// </returns>
+	private async Task<Guest[]> GetGuestsWithReservationId(int ReservationId)
+		=> (await _crud.GetAll()).Where(g => g.ReservationId == ReservationId).ToArray();
 
 	/// <summary> Api endpoint for getting a specific guest in the database.</summary>
 	/// <returns> 
@@ -38,6 +45,54 @@ public class GuestController : Controller
 	{
 		var guest = await _crud.TryGet(new HashSet<Key>(new Key[] { new(nameof(Guest.Id), Id) }));
 		return guest is null ? BadRequest(PersonNotFound) : Ok(guest);
+	}
+
+	/// <summary> Api endpoint for setting guests in the database. </summary>
+	/// <returns>
+	/// Status 200 (OK) with the new set of guests, when the guests has been set.
+	/// Status 400 (Bad request) with error message, when properties are invalid.
+	/// </returns>
+	[HttpPut($"[controller]/[action]/{{{nameof(Guest.ReservationId)}}}")]
+	public async Task<ObjectResult> Set([FromRoute]int ReservationId, [FromBody]Guest[]? guests)
+	{
+		// Get old set of guests
+		var reservationGuests = await GetGuestsWithReservationId(ReservationId);
+
+		// Delete all guests if new set is null
+		if(guests is null)
+		{
+			foreach (var delete in reservationGuests.Where(g => g.Id > -1))
+			{
+				if(delete.Id is not null) await Delete((int)delete.Id);
+			}
+		}
+		else
+		{
+			// Validate new guests
+			foreach (var guest in guests)
+			{
+				var result = _validator.Validate(guest);
+				if (!result.IsValid) return BadRequest(result.Errors);
+				guest.Reservation = null;
+			}
+			// Delete guests that are not in the new list
+			foreach (var delete in reservationGuests.Where(g => !guests.Any(g2 => g2.Id == g.Id) && g.Id >= 0))
+			{
+				if (delete.Id is not null) await Delete((int)delete.Id);
+			}
+			// Edit guests that are in the new list
+			foreach (var edit in guests.Where(g => reservationGuests.Any(g2 => g2.Id == g.Id)))
+			{
+				await Edit(edit);
+			}
+			// Add guests that are not in the old list
+			foreach (var add in guests.Where(g => reservationGuests.Any(g2 => g.Id < 0)))
+			{
+				await Create(add);
+			}
+		}
+		// Return new set of guests
+		return Ok(await GetGuestsWithReservationId(ReservationId));
 	}
 
 	/// <summary> Api endpoint for creating guests in the database. </summary>
