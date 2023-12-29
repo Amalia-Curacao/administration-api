eae0ac8dc045e44c0b440fb2ea64371b289745fc
Emile <emilemetry@outlook.com>
2023-12-29 18:15:07 -0400

    activate enpoints

--- Program.cs
+++ Program.cs
@@ -40,6 +40,7 @@ if (app.Environment.IsDevelopment())
 else{
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.MapControllers();
 }
 
 // TODO think about CORS
